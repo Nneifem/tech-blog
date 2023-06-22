c438e59e657e77eeba7dc2eba36b5a0d38b9cf25
Nneif <nneifemakpunonu@gmail.com>
2023-06-22 14:06:10 -0400

    added code to the server.js, connection.js, and to the .env

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3003;
+
+app.ues(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
