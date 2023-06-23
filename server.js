7bb3e0745c5d086c7728a88fee6a713d848834f1
Nneif <nneifemakpunonu@gmail.com>
2023-06-23 19:38:19 -0400

    added code to the login.handlebars and seeds.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const sequelize = require('./config/connection');
 const routes = require('./controllers');
 
 const app = express();
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 
-app.ues(express.json());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+    app.listen(PORT, () => console.log('App listening'));
 });
