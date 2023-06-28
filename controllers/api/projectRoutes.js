36c54f214944bba65853cf1111dbd0c1b9cad753
Nneif <nneifemakpunonu@gmail.com>
2023-06-27 22:55:49 -0400

    commented out code

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-        const projectData = await Project.findAll({
-            include: [{ model: Project }]
-        });
-        res.status(200).json(projectData)
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const projectData = await Project.findAll({
+//             include: [{ model: Project }]
+//         });
+//         res.status(200).json(projectData)
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.post('/', withAuth, async (req, res) => {
     try {
